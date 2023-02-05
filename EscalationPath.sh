9660d0502c04e41b6b76dfc5a95a80fe0c6ccfbe
Marc-Emmanuel MARTINO <63908456+Marc-Emmanuel9@users.noreply.github.com>
2023-02-05 13:02:35 +0100

    Update EscalationPath.sh

--- EscalationPath.sh
+++ EscalationPath.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+
 echo "EscalationPath.sh"
 echo ""
 echo "------------------------------------------"
-echo "Getting crontab"
+echo -e "${RED}Getting crontab${ENDCOLOR}"
 echo ""
 
 cat /etc/crontab
 
 echo ""
 echo "------------------------------------------"
-echo "Getting sudo -l"
+echo -e "${RED}Getting sudo -l${ENDCOLOR}"
 echo ""
 sudo -l
 
 echo ""
 echo "------------------------------------------"
-echo "Getting capabilities"
+echo -e "${RED}Getting capabilities${ENDCOLOR}"
 echo ""
 getcap -r / 2>/dev/null
 
 echo ""
 echo "------------------------------------------"
-echo "Getting SUID files"
+echo -e "${RED}Getting SUID files${ENDCOLOR}"
 echo ""
 
 find / -type f -perm -04000 -ls 2>/dev/null
 
 echo ""
 echo "------------------------------------------"
-echo "Getting NFS"
+echo -e "${RED}Getting NFS${ENDCOLOR}"
 echo ""
 
 cat /etc/exports
